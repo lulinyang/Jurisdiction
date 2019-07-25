59f6fbecccf4f587bad651e13aaa10a863a07869
lulinyang <15655569098@163.com>
2019-07-25 13:37:11 +0800

    getToken

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -41,7 +41,7 @@ class WeChatController extends BaseController
                 $token = $accessToken->getToken(true);
                 Redis::setex('access_token', ($token['expires_in'] - 100) ,$token['access_token']);
             } catch (\Throwable $th) {
-                dd($th);
+                dd($th->message);
             }
            
             $result = [
