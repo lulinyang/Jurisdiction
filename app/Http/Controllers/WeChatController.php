e54f5a3afab4895edaa845aac43270857e49b95e
lulinyang <15655569098@163.com>
2019-07-26 12:23:54 +0800

    wechat

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -20,24 +20,31 @@ class WeChatController extends BaseController
 
     public function serve()
     {
-        $postStr = file_get_contents("php://input", 'r');//php:input
-        error_log(var_export($_GET['echostr'],1),3,'php_log.txt');
+        
         if (!isset($_GET['echostr'])) {
-            $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-            if (!empty($postStr)){
-                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $msgType = trim($postObj->MsgType);
-                switch($msgType) {
-                    case 'event':
-                        $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'text':
-                        $this->transmitText($postObj, 'aaa');
-                        break;
-                }
-            }else {
-                echo "";exit;
-            }
+            //---------- 接 收 数 据 ---------- //
+  $postStr = $GLOBALS["HTTP_RAW_POST_DATA"]; //获取POST数据
+  //用SimpleXML解析POST过来的XML数据
+  $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+  $fromUsername = $postObj->FromUserName; //获取发送方帐号（OpenID）
+  $toUsername = $postObj->ToUserName; //获取接收方账号
+  $keyword = trim($postObj->Content); //获取消息内容
+  $time = time(); //获取当前时间戳
+  //---------- 返 回 数 据 ---------- //
+  //返回消息模板
+  $textTpl = "<xml>
+  <ToUserName><![CDATA[%s]]></ToUserName>
+  <FromUserName><![CDATA[%s]]></FromUserName>
+  <CreateTime>%s</CreateTime>
+  <MsgType><![CDATA[%s]]></MsgType>
+  <Content><![CDATA[%s]]></Content>
+  <FuncFlag>0</FuncFlag>
+  </xml>";
+  $msgType = "text"; //消息类型
+  $contentStr = 'hello'; //返回消息内容
+  //格式化消息模板
+  $resultStr = sprintf($textTpl,$fromUsername,$toUsername,$time,$msgType,$contentStr);
+  echo $resultStr; //输出结果
         }else{
             define("TOKEN", getenv('WECHAT_OFFICIAL_ACCOUNT_TOKEN'));
             $wechatObj = new WeChat();
