cdf52da05ada1e47011df925f84bf5749a03020e
lulinyang <15655569098@163.com>
2019-07-26 11:36:22 +0800

    wechat

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -21,42 +21,66 @@ class WeChatController extends BaseController
     public function serve()
     {
         if (!isset($_GET['echostr'])) {
-            $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-            if (!empty($postStr)){
-                $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $MsgType = trim($postObj->MsgType);
-                switch($MsgType) {
-                    case 'event':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'text':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'image':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'voice':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'video':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'location':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'link':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    case 'file':
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                    default:
-                        $result = $this->transmitText($postObj, 'aaa');
-                        break;
-                }
-            }else {
-                echo "";exit;
-            }
+            $postStr = $GLOBALS["HTTP_RAW_POST_DATA"]; //获取POST数据
+  //用SimpleXML解析POST过来的XML数据
+  $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+  $fromUsername = $postObj->FromUserName; //获取发送方帐号（OpenID）
+  $toUsername = $postObj->ToUserName; //获取接收方账号
+  $keyword = trim($postObj->Content); //获取消息内容
+  $masType = $postObj->MsgType;//获取消息类型
+  $time = time(); //获取当前时间戳
+  
+   //---------- 返 回 数 据 ---------- //
+  //返回消息模板
+   $textTpl = "<xml>
+    <ToUserName><![CDATA[%s]]></ToUserName>
+    <FromUserName><![CDATA[%s]]></FromUserName>
+    <CreateTime>%s</CreateTime>
+    <MsgType><![CDATA[%s]]></MsgType>
+    <Content><![CDATA[%s]]></Content>
+    <FuncFlag>0</FuncFlag>
+    </xml>"; 
+    
+    $msgType = "text"; //消息类型
+    $contentStr = '您发送的是'.$keyword;
+    $resultStr = sprintf($textTpl,$fromUsername,$toUsername,$time,$msgType,$contentStr);
+    echo $resultStr;
+            // $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+            // if (!empty($postStr)){
+            //     $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            //     $MsgType = trim($postObj->MsgType);
+            //     switch($MsgType) {
+            //         case 'event':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'text':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'image':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'voice':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'video':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'location':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'link':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         case 'file':
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //         default:
+            //             $result = $this->transmitText($postObj, 'aaa');
+            //             break;
+            //     }
+            // }else {
+            //     echo "";exit;
+            // }
         }else{
             define("TOKEN", getenv('WECHAT_OFFICIAL_ACCOUNT_TOKEN'));
             $wechatObj = new WeChat();
