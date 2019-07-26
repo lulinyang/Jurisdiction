4e67190d63660a68d924a5d08a2bb847c5e7d717
lulinyang <15655569098@163.com>
2019-07-26 12:56:58 +0800

    wechat

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -20,36 +20,38 @@ class WeChatController extends BaseController
 
     public function serve()
     {
-        
-        if (!isset($_GET['echostr'])) {
-            //---------- 接 收 数 据 ---------- //
-  $postStr = $GLOBALS["HTTP_RAW_POST_DATA"]; //获取POST数据
-  //用SimpleXML解析POST过来的XML数据
-  $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
-  $fromUsername = $postObj->FromUserName; //获取发送方帐号（OpenID）
-  $toUsername = $postObj->ToUserName; //获取接收方账号
-  $keyword = trim($postObj->Content); //获取消息内容
-  $time = time(); //获取当前时间戳
-  //---------- 返 回 数 据 ---------- //
-  //返回消息模板
-  $textTpl = "<xml>
-  <ToUserName><![CDATA[%s]]></ToUserName>
-  <FromUserName><![CDATA[%s]]></FromUserName>
-  <CreateTime>%s</CreateTime>
-  <MsgType><![CDATA[%s]]></MsgType>
-  <Content><![CDATA[%s]]></Content>
-  <FuncFlag>0</FuncFlag>
-  </xml>";
-  $msgType = "text"; //消息类型
-  $contentStr = 'hello'; //返回消息内容
-  //格式化消息模板
-  $resultStr = sprintf($textTpl,$fromUsername,$toUsername,$time,$msgType,$contentStr);
-  echo $resultStr; //输出结果
-        }else{
-            define("TOKEN", getenv('WECHAT_OFFICIAL_ACCOUNT_TOKEN'));
-            $wechatObj = new WeChat();
-            $wechatObj->valid();
-        }
+        $app = app('wechat.official_account');
+        $app->server->push(function ($message) {
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                    break;
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+        });
     }
 
     public function doEvent($postObj)
