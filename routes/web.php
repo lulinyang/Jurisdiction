6bc07cf4ab896d8cf3bfc9b6acac7beeadda7aed
lulinyang <15655569098@163.com>
2019-08-07 14:20:30 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/wxchat', function () {
+    return view('wxchat');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
