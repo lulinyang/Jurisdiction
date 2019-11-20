97e077c6708daf806e6f1e463b5cd57c63129a42
lulinyang <15655569098@163.com>
2019-11-20 23:38:08 +0800

    swoole

--- app/Http/Controllers/WebSocketController.php
+++ app/Http/Controllers/WebSocketController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 class WebSocketController extends Controller
 {
     //
-    public function test(Request $request)
+    public function test($params)
     {   
-        // var_dump($request->transId);
+        var_dump($params);
         echo 'aaa';
         // foreach ($server->connections as $fd){
         //     $server->push($fd,'aaaaaaaaaaa');
