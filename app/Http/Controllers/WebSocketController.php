a53ec7f70feba776a0931c5da407eea4a954e4ce
lulinyang <15655569098@163.com>
2019-11-20 23:34:41 +0800

    swoole

--- app/Http/Controllers/WebSocketController.php
+++ app/Http/Controllers/WebSocketController.php
@@ -9,7 +9,7 @@ class WebSocketController extends Controller
     //
     public function test(Request $request)
     {   
-        var_dump($request->transId);
+        // var_dump($request->transId);
         echo 'aaa';
         // foreach ($server->connections as $fd){
         //     $server->push($fd,'aaaaaaaaaaa');
