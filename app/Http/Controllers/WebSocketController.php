2ced4e31bfdc47d4b94a2e4aa5756e20b8469bac
lulinyang <15655569098@163.com>
2019-11-20 23:46:54 +0800

    swoole

--- app/Http/Controllers/WebSocketController.php
+++ app/Http/Controllers/WebSocketController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class WebSocketController extends Controller
 {
-    //
     public function test($server, $content)
     {   
-        // var_dump($params);
-        echo 'aaa';
-        // foreach ($server->connections as $fd){
-        //     $server->push($fd,'aaaaaaaaaaa');
-        // }
         // //推送给所有链接
         foreach ($server->connections as $fd){
             $server->push($fd,$content);
