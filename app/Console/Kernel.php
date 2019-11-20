1a67d0c6f79e000abd79dc9c4d85dd673be6b675
lulinyang <15655569098@163.com>
2019-11-20 16:43:40 +0800

    swoole

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\RepositoryMakeCommand::class
+        Commands\RepositoryMakeCommand::class,
+        Commands\Swoole::Class
     ];
 
     /**
